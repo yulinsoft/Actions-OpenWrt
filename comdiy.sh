a12c935edb7ee43a8580a3f840d08ebabf829cc7
yulinsoft <yulinsoft@vip.qq.com>
2020-01-29 12:22:35 +0800

    Create comdiy.sh

---
+++ comdiy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Delete Lean password
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
+echo 'modify completed.'
