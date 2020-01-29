8cd3e622b9964583965d342ef6541969d9b4760f
yulinsoft <yulinsoft@vip.qq.com>
2020-01-29 20:04:47 +0800

    Update comdiy.sh

--- comdiy.sh
+++ comdiy.sh
@@ -9,4 +9,6 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # Delete Lean password
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
+# frpc 0.21.0
+sed -i 's/0.31.1/0.21.0/g' package/lean/frpc/Makefile
 echo 'modify completed.'
