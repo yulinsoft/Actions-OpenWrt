7f3f930783a80235af1d79011a4cac752432ed3f
yulinsoft <yulinsoft@vip.qq.com>
2020-12-16 20:45:26 +0800

    添加feed

--- comdiy.sh
+++ comdiy.sh
@@ -9,6 +9,4 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # Delete Lean password
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
-# add feed helloworld
-sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 echo 'modify completed.'
