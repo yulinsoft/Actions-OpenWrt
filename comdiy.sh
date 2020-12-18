88d7a5f7dfd8048cd4ac9fc48548a687d1d3bc1e
yulinsoft <yulinsoft@vip.qq.com>
2020-12-18 09:52:22 +0800

    更新配置文件

--- comdiy.sh
+++ comdiy.sh
@@ -9,4 +9,7 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # Delete Lean password
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/iptables/d' package/lean/default-settings/files/zzz-default-settings
+# Modify kernel to 4.14
+sed -i 's/5.4/4.14/g' target/linux/ramips/Makefile
 echo 'modify completed.'
